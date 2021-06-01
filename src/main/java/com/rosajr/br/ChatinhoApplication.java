c1f1ccfbccceda4e7eeeeb261bd958b4a8a1c21c
João Rosa <joao.junior@itbam.org.br>
2021-06-01 15:06:06 -0400

    feat: open api config

---
+++ src/main/java/com/rosajr/br/ChatinhoApplication.java
@@ -0,0 +1,14 @@
+package com.rosajr.br;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosajr/br/ChatinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosajr.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
